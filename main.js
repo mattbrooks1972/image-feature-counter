4310fb4fb1bb869f86fb01c6e96cfd802180f065
Matt Brooks <mattbrooks1972@live.com>
2021-04-17 18:29:48 -0400

    Add canvas rendering

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 document.getElementById('image-input').addEventListener('input', display_image);
 document.getElementById('image-display').addEventListener('load', fit_image_to_canvas);
 
+let canvas = document.getElementById('image-canvas');
+let ctx = canvas.getContext('2d');
+
 function display_image(event) {
 	let imageDisplay = document.getElementById('image-display');
 	imageDisplay.src = URL.createObjectURL(event.target.files[0]);
