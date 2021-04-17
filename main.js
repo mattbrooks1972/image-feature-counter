ceb2232974cb7e8ceee2f1e27bfb310662b986a6
Matt Brooks <mattbrooks1972@live.com>
2021-04-17 18:17:13 -0400

    Add event listener to input image load

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.getElementById('image-input').addEventListener('input', display_image);
+document.getElementById('image-display').addEventListener('click', fit_image_to_canvas);
 
 function display_image(event) {
 	let imageDisplay = document.getElementById('image-display');
